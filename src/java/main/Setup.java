65fd7b1375449a717ebeaf9b7a760265826b3c6b
ryank111 <70592671+ryank111@users.noreply.github.com>
2022-06-16 11:20:16 -0400

    Moved main start

---
+++ src/java/main/Setup.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Setup {
+    public void startScreen(){
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
+}
