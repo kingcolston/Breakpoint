43d1818756b0b1610266b49d76095fab33216941
ryank111 <70592671+ryank111@users.noreply.github.com>
2022-06-16 13:40:32 -0400

    added username and password

--- src/java/main/Setup.java
+++ src/java/main/Setup.java
@@ -7,6 +7,7 @@ public class Setup {
         frame = new JFrame("Breakpoint");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
+        //frame.setLayout(new FlowLayout());
         //Creating the MenuBar and adding components
         JMenuBar mb = new JMenuBar();
         JMenu m1 = new JMenu("FILE");
@@ -19,46 +20,64 @@ public class Setup {
         m1.add(m22);
 
         //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
+        JPanel username = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Username");
         JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
+        username.add(label); // Components Added using Flow Layout
+        username.add(tf);
 
-        // Text Area at the Center
-        //TextArea ta = new JTextArea();
+        JPanel password = new JPanel(); // the panel is not visible in output
+        JLabel labelPassword = new JLabel("Password");
+        JTextField tfPassword = new JTextField(10); // accepts upto 10 characters
+        password.add(labelPassword); // Components Added using Flow Layout
+        password.add(tfPassword);
+
+        JButton reset = new JButton("Reset");
+        JButton send = new JButton("Send");
 
         //Middle
-        /*
-        JPanel middle = new JPanel();
-        JLabel header = new JLabel("Breakpoint");
-        header.setFont(new Font("Times", Font.BOLD, 20));
-        middle.add(header);
 
-        JTextArea description = new JTextArea("Welcome to our humble abode. I hope you have a good time here and gets lots of work done. Select preferences below.");
-        description.setLineWrap(true);
-        middle.add(description);*/
-
+        Panel middleBox = new Panel();
+        Panel middleTextPanel = new Panel();
         JLabel middle = new JLabel(
                 "<html>" +
                         "<div style = \"background-color: #ADD8E6; margin: 20px\">" +
                         "<h1 style = \"text-align: center\">" +
                         "Breakpoint" +
                         "</h1>" +
-                        "<div style = \"text-align: center; padding: 10px 30px\">" +
+                        "<p style = \"text-align: center; padding: 10px 30px; display: flex; flex-wrap: wrap\">" +
                         "Welcome to our humble abode. I hope you have a good time here and gets lots of work done. Select preferences below." +
-                        "</div>" +
+                        "</p>" +
                         "</div>" +
                         "</html>");
+        //tf.setSize(5,5);
+
+        middleBox.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        middleBox.add(middle, gbc);
+
+        gbc.weightx = 0.5;
+        gbc.gridy = 1;
+        middleBox.add(username, gbc);
+
+        gbc.gridy = 2;
+        middleBox.add(password, gbc);
+
+        gbc.gridy = 3;
+        middleBox.add(send, gbc);
+        gbc.gridx = 1;
+        middleBox.add(reset, gbc);
 
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        //frame.getContentPane().add(BorderLayout.SOUTH, panel);
         frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, middle);
+        frame.getContentPane().add(BorderLayout.CENTER, middleBox);
         frame.setVisible(true);
+
     }
 }
