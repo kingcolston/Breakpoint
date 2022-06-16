4dc078e43d451e3113092836b7babfc7da18d7ee
Madhava <vmadhavarao13@gmail.com>
2022-06-16 12:07:11 -0400

    spring app pus1

---
+++ breakpoint_backend/src/main/java/com/example/breakpoint/breakpoint/BreakpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.breakpoint.breakpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakpointApplication.class, args);
+	}
+
+}
---
+++ breakpoint_backend/src/test/java/com/example/breakpoint/breakpoint/BreakpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.breakpoint.breakpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
