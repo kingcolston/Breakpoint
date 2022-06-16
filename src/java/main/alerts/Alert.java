9a24b1d5b5aa58f09ec7e9ca10c041836266ef75
juliabutler <juliafulibutler@gmail.com>
2022-06-16 11:14:41 -0400

    Redesigning alert class

---
+++ src/java/main/alerts/Alert.java
@@ -0,0 +1,24 @@
+package alerts;
+
+public class Alert {
+    public String getAlertName() {
+        return alertName;
+    }
+
+    public void setAlertName(String alertName) {
+        this.alertName = alertName;
+    }
+
+    public String getAlertMessage() {
+        return alertMessage;
+    }
+
+    public void setAlertMessage(String alertMessage) {
+        this.alertMessage = alertMessage;
+    }
+
+    public String alertName;
+    public String alertMessage;
+
+
+}
